87dc429d07e328aeac8e569cfdd6284eb177320b
AngelMoya <amoyapardo@gmail.com>
2016-03-08 18:06:16 +0100

    ADD many2many fielf view

--- todo_app/models/todo_app.py
+++ todo_app/models/todo_app.py
@@ -2,6 +2,29 @@
 
 from openerp import models, fields
 
+class TodoAppCategory(models.Model):
+    _name = 'todo.app.category'
+
+    name = fields.Char(
+        string="Name",
+        required=True,
+        translate=True)
+    todo_ids = fields.Many2many(
+        comodel_name='todo.app.category',
+        relation='todo_app_category_rel',
+        column1='categ_id',
+        column2='todo_id',
+        string='Categories')
+
+
+class TodoAppRev(models.Model):
+    _name = 'todo.app.rev'
+
+    name = fields.Char(
+        string="Tittle",
+        required=True,
+        translate=True)
+
 class TodoApp(models.Model):
     _name = 'todo.app'
 
@@ -17,6 +40,14 @@ class TodoApp(models.Model):
     type = fields.Selection(
         [('normal','Normal'),
          ('prior','Prior')],
-        string="Type")
+        string="Type",
+        default="normal",
+        required=True)
     # etiquetas m2m
+    category_ids = fields.Many2many(
+        comodel_name='todo.app.category',
+        relation='todo_app_category_rel',
+        column1='todo_id',
+        column2='categ_id',
+        string='Categories')
     # revisiones o2m 
