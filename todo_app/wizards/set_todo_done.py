56f4863321108b71b04b440945a342f866592468
AngelMoya <amoyapardo@gmail.com>
2016-03-09 18:03:15 +0100

    IMP wizard

--- todo_app/wizards/set_todo_done.py
+++ todo_app/wizards/set_todo_done.py
@@ -9,6 +9,7 @@ class TodoSetDone(models.TransientModel):
         string="Rev")
     todo_id = fields.Many2one(
         comodel_name='todo.app',
+        required=True,
         string='Todo')
     user_id = fields.Many2one(
         comodel_name='res.users',
@@ -21,9 +22,19 @@ class TodoSetDone(models.TransientModel):
         values = {
             'name': self.name,
             'todo_id': self.todo_id.id,
-            'user_id': self.user_id.id}
+            'user_id': self.user_id and self.user_id.id}
         rev_id = app_rev_obj.create(values)
         # pasar el todo a estado done
         #self.todo_id.write({'state':'done'})
         #self.todo_id.state = 'done'
         self.todo_id.set_done_state()
+    
+    @api.model
+    def default_get(self, fields):
+
+        res = super(TodoSetDone, self).default_get(fields)
+        
+        res.update({
+            'todo_id':self.env.context.get('active_id'),
+            'user_id':self.env.context.get('uid'),})
+        return res
