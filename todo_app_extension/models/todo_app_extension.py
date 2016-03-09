a2e4d747d077391a8ad368bd3c4736e4edd00ec0
AngelMoya <amoyapardo@gmail.com>
2016-03-09 19:32:14 +0100

    WIP module extension

--- todo_app_extension/models/todo_app_extension.py
+++ todo_app_extension/models/todo_app_extension.py
@@ -3,5 +3,10 @@
 from openerp import models, fields
 
 # Añadir partner_id a todo
+class TodoApp(models.Model):
+    _inherit = 'todo.app'
 
+    # m2o 'res.partner'
+    
+    
 # Añadir todo_ids a partner
